23617ad8921699b8f9d9dc414c8bdf00e1dc3c89
vickywang410 <vickywang410@gmail.com>
2016-10-26 16:05:45 -0400

    Initial commit

---
+++ FRC Rookie 101/FRC Rookie 101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FRC Rookie 101
+//
+//  Created by Vicky Wang on 2016-10-26.
+//  Copyright © 2016 Vicky Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
